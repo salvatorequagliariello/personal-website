647f0f02b41ff68a9bec7eb8f7c6ce2e7412fa6e
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-08 16:46:58 +0200

    complete work  component

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 import PageNavigator from "./PageNavigator"
 
 const ProjectPreview = ({projectTitle, projectDescription, projectLink, projectTech, projectImg}) => {
@@ -6,14 +8,22 @@ const ProjectPreview = ({projectTitle, projectDescription, projectLink, projectT
             <PageNavigator path={"/work"} page={projectTitle} />
             <div className="project-preview__container content-container">
                 <div>
-                    <h1 className="project-title p-font-xl fill-txt">{projectTitle}</h1>
-                    <p className="p-font-sm fill-txt">{projectDescription}</p>
+                    <div className="project-title__container">
+                        <h1 className="project-title p-font-xl fill-txt">{projectTitle}</h1>
+                        <a 
+                        className="fill-txt hoverable" 
+                        href={projectLink}
+                        target="_blank">
+                            <FontAwesomeIcon className="project-title__icon fill-txt hoverable" icon={faLink} />
+                        </a>
+                    </div>
+                    <img  className="project-image" src={projectImg} />
                 </div>
+                    <p className="p-font-sm fill-txt">{projectDescription}</p>
                 <div>
                     <p className="project-stack p-font-md fill-txt">TECH STACK</p>
                     <p className="p-font-sm fill-txt">{projectTech}</p>
                 </div>
-                <img src={projectImg}></img>
             </div>
         </div>
     );
