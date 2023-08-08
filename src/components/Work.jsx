6a0f5328bd9858100bc756da0b441695d3cd689e
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-08 15:13:09 +0200

    add work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
 import PageNavigator from "./PageNavigator";
 
 const Work = () => {
     return (
         <div className="animate__animated animate__fadeIn">
             <PageNavigator path={"/"} page={"WORK"} />
+            <div className="nav">
+                <nav className="work-nav">
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Instagram</Link>
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Facebook</Link>
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Linkedin</Link>
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Youtube</Link>
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Snapchat</Link>
+                    <Link className="work-nav__item h-font-sm fill-txt hoverable">Ebay</Link>
+                </nav>
+            </div>
         </div>
     );
 };
