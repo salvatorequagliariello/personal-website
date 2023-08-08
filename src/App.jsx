357c024fd48ca9c4a215bc76951954acc3c309b0
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-08 17:49:37 +0200

    add about component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AnimationBg from "./background-animation/animation-script"
 import Home from "./components/Home";
 import Work from "./components/Work";
 import ProjectTest from "./components/projects/ProjectTest";
+import About from "./components/About";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
+          <Route path="/about" element={<About />} />
           <Route path="/work/instagram" element={<ProjectTest />} />
         </Routes>
       </div>
