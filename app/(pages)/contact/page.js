8e8534fa509a27fd83e37ca1ca18adffefd9d4ee
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-11-17 11:28:50 +0100

    add contact form

--- app/(pages)/contact/page.js
+++ app/(pages)/contact/page.js
@@ -36,11 +36,68 @@ const Contact = () => {
                 });
         }
     });
-    
+
     return (
         <div>
             <CustomNavigator pageName='contact' path='/' pathName='home'/>
-            <p>Contact</p>
+            <div className="text-center font-light">
+                <h1 className="text-5xl">Let's work together!</h1>
+                <p className="text-2xl">(or just say "hi"!)</p>
+            </div>
+            {!formSubmission.submitted && formSubmission.errors.length == 0 && 
+                <form onSubmit={formik.handleSubmit} id="contactForm"  className="flex flex-col gap-7 mt-7">
+                    <div className="w-full flex justify-center gap-20">
+                        <div className="w-full">
+                            <label className={`font-light text-xl ${formik.touched.name && formik.errors.name ? " text-orange-500" : ""}`}>
+                                Name
+                            </label>
+                            <input 
+                            type="text" 
+                            name="name" 
+                            placeholder="Ada Lovelace" 
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            className={`w-full p-3 text-secondary dark:text-primary bg-primary dark:bg-secondary outline-0 ${formik.touched.name && formik.errors.name ? "border-2 border-orange-500" : ""}`}
+                            />
+                            {formik.touched.name && formik.errors.name && <p className="font-light text-orange-500">{formik.errors.name}</p>}
+                        </div>
+                        <div className="w-full">
+                            <label className={`font-light text-xl ${formik.touched.email && formik.errors.email ? " text-orange-500" : ""}`}>
+                                Email
+                            </label>
+                            <input 
+                            type="text" 
+                            name="email" 
+                            placeholder="ada@lovelace.com"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            className={`w-full p-3 text-secondary dark:text-primary bg-primary dark:bg-secondary outline-0 ${formik.touched.email && formik.errors.email ? "border-2 border-orange-500" : ""}`}
+                            />
+                            {formik.touched.email && formik.errors.email && <p className="font-light text-orange-500">{formik.errors.email}</p>}
+                        </div>
+                    </div>
+
+                    <div className="w-full">
+                        <label className={`font-light text-xl ${formik.touched.message && formik.errors.message ? " text-orange-500" : ""}`}>
+                            Message
+                        </label>
+                        <textarea 
+                        name="message" 
+                        rows="4" 
+                        placeholder="I'd really like to work with you!"
+                        value={formik.values.message}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={`w-full h-64 p-3 text-secondary dark:text-primary bg-primary dark:bg-secondary outline-0 ${formik.touched.message && formik.errors.message ? "border-2 border-orange-500" : ""}`}
+                        />
+                        {formik.touched.message && formik.errors.message && <p className="font-light text-orange-500">{formik.errors.message}</p>}
+                    </div>
+
+                        <button type="submit" className="form-button p-font-md fill-txt hoverable">SEND</button>
+                </form>
+            }
         </div>
     )
 }
