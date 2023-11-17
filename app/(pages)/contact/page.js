6bee69192541860f305ae5b37276051edda75427
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-11-17 09:43:44 +0100

    add contact form logic

--- app/(pages)/contact/page.js
+++ app/(pages)/contact/page.js
@@ -7,6 +7,36 @@ import * as Yup from "yup";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+    const formData = {
+        submitted: false,
+        errors: [],
+    }
+    const [formSubmission, setFormSubmission] = useState(formData);
+
+    const formik  = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().required(`Please, enter your name.`),
+            email: Yup.string().email(`Please, enter a valid email address.`).required(`Please, enter a valid email address.`),
+            message: Yup.string().required(`Would you really send an empty message? :c`)
+        }),
+        onSubmit: (values, onSubmitProps) => {
+                emailjs.sendForm("service_uf8c07b", 'template_br8of41', ".contact-form", '1eM3d97JibQ3jXaV0')
+                .then(response => {
+                    onSubmitProps.setSubmitting(false);
+                    onSubmitProps.resetForm();
+                    setFormSubmission({...formSubmission, submitted: true});
+                })
+                .catch(error => {            
+                    setFormSubmission({...formSubmission, submitted: false, errors: [error]});
+                });
+        }
+    });
+    
     return (
         <div>
             <CustomNavigator pageName='contact' path='/' pathName='home'/>
