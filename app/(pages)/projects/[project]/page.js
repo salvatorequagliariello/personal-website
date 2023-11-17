0cea472634ce9428eaa67785dbbe6c3ea5743905
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-11-17 17:08:06 +0100

    add projectslist constant

--- app/(pages)/projects/[project]/page.js
+++ app/(pages)/projects/[project]/page.js
@@ -1,35 +1,5 @@
 import CustomNavigator from "../../../components/CustomNavigator";
-
-const projects = [
-    {
-        name: 'Portfolio',
-        image: '/images/portfolio-screen.png',
-        liveLink: 'https:www.salvatorequagliariello.com',
-        githubLink: 'https:www.github.com',
-        tech: 'React, NextJS, Tailwind',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum ornare magna, at eleifend sapien faucibus eu. Etiam interdum velit vitae nunc vulputate, ac tempus metus dictum. Morbi eget ultrices velit. Nullam eu tortor est. Etiam sit amet diam vel libero hendrerit gravida. Aliquam sed posuere magna. Vivamus molestie ante eget tortor congue suscipit. Integer vulputate convallis sagittis. Aliquam fermentum ut urna lacinia posuere. Fusce facilisis aliquet eleifend. Cras condimentum bibendum eros eget blandit.'
-    },
-    {
-        name: 'Timeless',
-        image: '/images/timeless-screen.png'
-    },
-    {
-        name: 'ai.GO',
-        image: '/images/aigo-screen.png'
-    },
-    {
-        name: 'eCommerce CRM',
-        image: '/images/crm-screen.png'
-    },
-    {
-        name: 'uChat',
-        image: '/images/uchat-screen.png'
-    },
-    {
-        name: 'Lotto Game',
-        image: '/images/lotto-screen.png'
-    },
-]
+import projects from 'app/projectslist.js'
 
 const ProjectPage = ({ params }) => {
     const project = projects.filter(project => project.name == params.project.replace(/%20/g, ' '))[0];
