7fcfc2c355c8e249b3e9e94c4e657c466d8489e7
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-12-06 16:33:40 +0100

    modify link buttons in project page

--- app/(pages)/projects/[project]/page.js
+++ app/(pages)/projects/[project]/page.js
@@ -10,27 +10,42 @@ const ProjectPage = ({ params }) => {
             <div className="flex flex-col gap-5 animate-fade-up animate-once animate-delay-900 animate-normal animate-fill-forwards">
                 <h1 className="text-4xl md:text-6xl font-light text-center">{project.name}</h1>
                 <img src={project.image} className="w-full h-56 object-cover sm:h-72 md:h-80 md:object-none" />
-                <div className="w-full h-min flex flex-col gap-3 text-center md:items-baseline md:flex-row md:justify-between md:text-start">
+                <div className="w-full h-min flex flex-col gap-3">
+                    <div className="w-min mx-auto flex gap-3 justify-center">
+                        <a href={project.githubLink} target="_blank" className="flex h-min gap-2 hover:bg-accentred bg-primary dark:bg-secondary p-2 group">
+                                <p className="text-secondary dark:text-primary">
+                                    GitHub
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6 sm:w-7 sm:h-7 fill-none stroke-secondary dark:stroke-primary">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                                </svg>
+                        </a>
+                        <a href={project.githubLink} target="_blank" className="flex h-min gap-2 hover:bg-accentred bg-primary dark:bg-secondary p-2 group">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6 sm:w-7 sm:h-7 fill-none stroke-secondary dark:stroke-primary">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                            </svg>
+                            <p className="text-secondary dark:text-primary">
+                                Website
+                            </p>
+                        </a>
+                    </div>
                     <div className="text-xl">
                         <h3 className="inline">Tech stack: </h3>
                         <p className="font-light inline">{project.tech}</p>
                     </div>
-                    <div className="w-full flex gap-3 justify-center md:w-min md:justify-normal">
-                        <a href={project.githubLink} target="_blank" className="hover:bg-accentred bg-primary dark:bg-secondary p-2 group hov-txt">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6 sm:w-7 sm:h-7 fill-none stroke-secondary dark:stroke-primary">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
-                            </svg>
-                        </a>
-                        <a href={project.liveLink} target="_blank" className="hover:bg-accentred bg-primary dark:bg-secondary p-2 group hov-txt">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6 sm:w-7 sm:h-7 fill-none stroke-secondary dark:stroke-primary">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-                            </svg>
-                        </a>
-                    </div>
                 </div>
-                <p className="text-justify">
-                    {project.description}
-                </p>
+                {project.descriptionOne && <p className="text-justify">
+                    {project.descriptionOne}
+                </p>}
+                {project.descriptionTwo && <p className="text-justify">
+                    {project.descriptionTwo}
+                </p>}
+                {project.descriptionThree && <p className="text-justify">
+                    {project.descriptionThree}
+                </p>}
+                {project.descriptionFour && <p className="text-justify">
+                    {project.descriptionFour}
+                </p>}
             </div>
         </div>
     )
--- app/projectslist.js
+++ app/projectslist.js
@@ -11,10 +11,13 @@ const projects = [
     {
         name: 'Timeless',
         image: '/images/timeless-screen.png',
-        liveLink: 'https:www.salvatorequagliariello.com',
-        githubLink: 'https:www.github.com',
-        tech: 'React, NextJS, Tailwind',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum ornare magna, at eleifend sapien faucibus eu. Etiam interdum velit vitae nunc vulputate, ac tempus metus dictum. Morbi eget ultrices velit. Nullam eu tortor est. Etiam sit amet diam vel libero hendrerit gravida. Aliquam sed posuere magna. Vivamus molestie ante eget tortor congue suscipit. Integer vulputate convallis sagittis. Aliquam fermentum ut urna lacinia posuere. Fusce facilisis aliquet eleifend. Cras condimentum bibendum eros eget blandit.',
+        liveLink: 'https://timeless-watches.vercel.app/',
+        githubLink: 'https://github.com/salvatorequagliariello/timeless-shop',
+        tech: 'TypeScript, React, NextJS, TailwindCSS, Vercel, Stripe',
+        descriptionOne: 'The TIMELESS E-shop is a responsive web-app built with NextJS, React and written in TypeScript, so it can be easily refactored or manteined. The entire application is managed by my CMS, which send data about products, categories and billboards to the website via API.',
+        descriptionTwo: 'The styling of the website is based on TailwindCSS, and in addition to being fully responsive, there is a Light/Dark theme mode that automatically sets itself according to the preferences of the operating system on which it is displayed.',
+        descriptionThree: 'Once on the e-commerce it is possible to browse through the different collections or filter search results by material, strap material, or movement type.',
+        descriptionFour: 'It is then possible to click on a specific timepiece to see its history and features in more detail, admire its case, or simply to purchase it. The E-commerce shopping cart allows customers to "buy" the watches they choose, thanks to the integration with Stripe, which will fully simulate the purchase phase.',
         pathName: 'Timeless'
     },
     {
